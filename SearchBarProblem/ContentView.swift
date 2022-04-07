1bfaf3f3a0081da6e143f8de5805a0a98369e244
Gene Bogdanovich <genebogdanovich@gmail.com>
2022-04-07 13:46:05 +0300

    Initial Commit

---
+++ SearchBarProblem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchBarProblem
+//
+//  Created by Gene Bogdanovich on 7.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchBarProblem/SearchBarProblemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarProblemApp.swift
+//  SearchBarProblem
+//
+//  Created by Gene Bogdanovich on 7.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SearchBarProblemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
